d6cdac502c96a7a21441cd2eed31c23ff3311959
Anna Valerio <anna.j.neff@gmail.com>
2024-06-24 10:58:37 -0500

    Loop Chapter Examples

--- loops/chapter-examples/for-Loop-Practice-With-Arrays.js
+++ loops/chapter-examples/for-Loop-Practice-With-Arrays.js
@@ -1,3 +1,9 @@
 // create an array variable containing the names
 
+let family = ["AJ", "Miguel", "Felix", "Jim", "Dinah", "Michelle", "Susie"];
+
 // write a for loop that prints each name on a different line
+
+for (i = 0; i < family.length; i++){
+    console.log(family[i]);
+}
--- loops/chapter-examples/for-Loop-Practice-With-Strings.js
+++ loops/chapter-examples/for-Loop-Practice-With-Strings.js
@@ -1,4 +1,8 @@
 // Create a string variable containing your name.
-
+ let name1 = "Anna Jeanne"
 
 // Write a for loop that prints each character in your name on a different line.
+
+for (i = 0; i < (name1.length); i++){
+    console.log(name1[i]);
+}

b8a2ccfd2ad6b1a527aecb2de1b33a8cb8ef2796
Anna Valerio <anna.j.neff@gmail.com>
2024-08-02 15:13:53 -0500

    DOM and Events exercises complete

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -4,7 +4,22 @@ function init () {
     const paragraph = document.getElementById("statusReport");
 
     // Put your code for the exercises here.
-    
+    button.addEventListener("click", event => {
+        paragraph.innerHTML = "Houston, we have liftoff!";
+    });
+
+    missionAbort.addEventListener("mouseover", function() {
+        missionAbort.style.backgroundColor = 'red';
+    });
+    missionAbort.addEventListener("mouseout", function(){
+        missionAbort.style.backgroundColor = "";
+    });
+    missionAbort.addEventListener("click", function(){
+        let abortConfirm = window.confirm("Are you sure you want to abort the mission?");
+        if (abortConfirm = true){
+            paragraph.innerHTML = "Mission aborted! Space shuttle returning home"
+        }
+    })
 }
 
 window.addEventListener("load", init);
